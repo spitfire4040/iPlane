d6f44ad6c6082bf3225911545c322902f8db0d06
Jay <jthom@cse.unr.edu>
2016-06-27 12:21:14 -0700

    Update files

--- iplane.cpp
+++ iplane.cpp
@@ -1,49 +1,59 @@
-/*
-import sys
-import os
-import urllib
-
-for x in range(1, 31):
-	f = open("/home/jay/iPlane/iPlane_traces/" + str(x) + "/traces.txt", "r")
-
-	if (x < 10):
-		day = "0" + str(x)
-	else:
-		day = str(x)
-
-	out = open("/home/jay/Desktop/iplane_test/Day-" + day, "w")
-
-	for line in f:
-
-		url = "http://iplane.cs.washington.edu/data/iplane_logs/2016/04/" + day + "/" + line
-
-		urllib.urlretrieve(url, "/home/jay/iPlane/temp/temp.txt")
-*/
-
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <stdio.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
+void truncate_error(char *filename);
+
+void decode(char* filename);
+
+
 int main()
 {
-	string day, num;
+	string day, num, line, temp, instr;
+    ifstream fin;
+    char* call;
+    char fname[] = {"temp.gz"};
 
 	for (int x = 1; x < 31; x++)
 	{
+        stringstream ss;
+        ss << x;
+
 		if (x < 10)
 		{
-			num = to_string(x);
+			num = ss.str();
 			day = "0" + num;
 		}
 		else
 		{
-			day = to_string(x);
+			day = ss.str();
 		}
-		cout << day << endl;
-	}
 
+        fin.open("iPlane_traces/" + day + "/traces.txt");
+
+        while (getline(fin, temp))
+        {
+        	instr = "wget http://iplane.cs.washington.edu/data/iplane_logs/2016/04/01/" + temp + " -O temp.gz";
+        	system(instr.c_str());
+        	system("gzip -dc temp.gz > temp.out");
+        	system("./a.out temp.out > temp.txt");
+        	system("python iplane.py");
+        }
+
+        fin.close();
+	}
 
 	return 0;
 }
--- iplane.py
+++ iplane.py
@@ -1,19 +1,72 @@
+# import headers
 import sys
 import os
-import urllib
 
-for x in range(1, 31):
-	f = open("/home/jay/iPlane/iPlane_traces/" + str(x) + "/traces.txt", "r")
+# initialize variables
+trace = []
+traces = []
+hop = 1
+flag = False
+src = ''
+dst = ''
 
-	if (x < 10):
-		day = "0" + str(x)
+# open input file
+f = open("temp.txt", "r")
+
+# open number file
+numfile = open("num.txt", "r")
+
+# read number file and close
+num = numfile.read()
+numfile.close()
+
+# iterate through lines of f
+for line in f:
+	line = line.split()
+
+	# if 'destination', write and reset
+	if (line[0] == "destination:"):
+		dst = line[1]
+
+		# get rid of empty line
+		if trace != None:
+			traces.append(trace)
+
+		# reset lists and flag
+		trace = []
+		hop = 1
+		flag = False
 	else:
-		day = str(x)
+		if line[1] == "0.0.0.0":
+			addr = '0'
+		else:
+			addr = line[1]
 
-	out = open("/home/jay/Desktop/iplane_test/Day-" + day, "w")
+		# print src:dst on first pass
+		if (flag == False):
+			trace.append(addr + ':' + dst + ' ')
+			trace.append(addr + '-' + str(hop) + ' ')
+			flag = True
+			hop += 1		
+		else:
+			trace.append(addr + '-' + str(hop) + ' ')
+			hop += 1
+f.close()
 
-	for line in f:
+# open output file
+out = open("all_trace_" + num + ".txt", "w")
 
-		url = "http://iplane.cs.washington.edu/data/iplane_logs/2016/04/" + day + "/" + line
+# write list to file
+for item in traces:
+	if not item:
+		pass
+	else:
+		out.write(''.join(item) + '\n')
+out.close()
 
-		urllib.urlretrieve(url, "/home/jay/iPlane/temp/temp.txt")
+# reset number file for next time
+filenum = int(num)
+filenum += 1
+numfile = open("num.txt", "w")
+numfile.write(str(filenum))
+numfile.close()
